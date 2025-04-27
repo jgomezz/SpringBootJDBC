6115c1f8c675b51dcc3ee5dc5fe269c529d3f121
jgomezz <jgomezz@gmail.com>
2025-04-27 11:17:50 -0500

    update

--- src/main/java/pe/edu/tecsup/springbootapp/entities/Categoria.java
+++ src/main/java/pe/edu/tecsup/springbootapp/entities/Categoria.java
@@ -1,5 +1,9 @@
 package pe.edu.tecsup.springbootapp.entities;
 
+
+import lombok.Data;
+
+@Data
 public class Categoria {
 
     private Long id;
---
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepositoryImpl.java
@@ -0,0 +1,22 @@
+package pe.edu.tecsup.springbootapp.repositories;
+
+import org.springframework.stereotype.Repository;
+import pe.edu.tecsup.springbootapp.entities.Categoria;
+
+import java.util.List;
+
+@Repository
+public class CategoriaRepositoryImpl implements CategoriaRepository {
+
+    @Override
+    public List<Categoria> findAll() throws Exception {
+
+        Categoria cat = new Categoria();
+
+        cat.setNombre("Robot");
+        String sc = cat.getNombre();
+
+        return List.of();
+    }
+
+}
